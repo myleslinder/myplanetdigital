e4f5d023a5913e12c490ecc67d7c163080cf1755
Ihor Marusyk <ih2502mk@gmail.com>
2014-02-25 16:09:50 -0500

    changed git repo protocol

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@
 
 export GH_REPO="myplanetdigital/swat"
 echo "Set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
-export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
+export REPO_URL="git://$GH_TOKEN@github.com/$GH_REPO.git"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 git branch -a
