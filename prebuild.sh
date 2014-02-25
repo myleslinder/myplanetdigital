d2cd5eeae9433a70e3099349c563891f2ce339d7
Ihor Marusyk <ih2502mk@gmail.com>
2014-02-25 17:33:13 -0500

    another attempt to make travis push it

--- prebuild.sh
+++ prebuild.sh
@@ -4,16 +4,9 @@ echo "Set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
 export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
-git config credential.helper "store --file=.git/credentials"
-echo "https://$GH_TOKEN@github.com" > .git/credentials
-git branch -a
-echo "STATUS"
-git status
-git remote rename origin old
-echo "remotes pre pre-authorized remote url"
-git remote -v
+
+git remote rm origin
 git remote add origin $REPO_URL
-git config remote.origin.url $REPO_URL
 
 #echo "DEBUG, cd out"
 #test -d out && (
