f933b86ff3ecf199a28daf6ff35e626ad0ae2c93
Ihor Marusyk <ih2502mk@gmail.com>
2014-03-14 15:13:11 -0400

    banner updates

--- src/files/scripts/modules/banner.js
+++ src/files/scripts/modules/banner.js
@@ -1,14 +1,14 @@
 (function banner() {
   $window = $(window);
   $menu = $('#menu');
-  $banner = $('.banner > .banner-text');
+  $bannerText = $('.banner > .banner-text');
 
   $window.on('filter', handleBanner);
 
   function handleBanner() {
     var pathTo = window.location.pathname;
     var bannerText = $('#menu').find('a[href="'+pathTo+'"]').data('banner');
-    $banner.html(bannerText);
+    $bannerText.html(bannerText);
   }
 
 }())
