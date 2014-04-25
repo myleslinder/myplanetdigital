1b95bde4d68bd7f4b594f077b3cbbbebb7b88144
neilbaylorrulez <neil.t@myplanetdigital.com>
2014-04-25 15:44:03 -0400

    Wrap tiles immediate in an IIAF

--- src/files/scripts/modules/tiles-immediate.js
+++ src/files/scripts/modules/tiles-immediate.js
@@ -1,23 +1,26 @@
-window.tiles = new Isotope( '.main-wrap', {
-  itemSelector: '.tile',
-  filter: '.visible',
-  masonry: {
-    columnWidth: '.grid-size'
-  },
-  transitionDuration: 0,
-  visibleStyle: {},
-  hiddenStyle: {}
-});
+(function tiles () {
+  'use strict';
+  window.tiles = new Isotope( '.main-wrap', {
+    itemSelector: '.tile',
+    filter: '.visible',
+    masonry: {
+      columnWidth: '.grid-size'
+    },
+    transitionDuration: 0,
+    visibleStyle: {},
+    hiddenStyle: {}
+  });
 
-var loadingGif = new Image();
+  var loadingGif = new Image();
 
-loadingGif.src = "/images/loading.gif";
-loadingGif.onload = function () {
-  if('ontouchstart' in window) {
-      var els = document.querySelectorAll('.tile'),
-          len = els.length;
-      while(len--) {
-          els[len].style.opacity = 1;
-      }
-  }
-};
+  loadingGif.src = "/images/loading.gif";
+  loadingGif.onload = function () {
+    if('ontouchstart' in window) {
+        var els = document.querySelectorAll('.tile'),
+            len = els.length;
+        while(len--) {
+            els[len].style.opacity = 1;
+        }
+    }
+  };
+}());
